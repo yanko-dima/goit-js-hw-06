9b8499f224fc2e7dff79c9db3522889980902189
Yanko Dima <yanko.dmitriy@gmail.com>
2022-04-08 23:46:46 +0300

    Task 2 Do

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,25 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulEl = document.getElementById("ingredients");
+console.log(ulEl);
+
+const onListItemElement = (ingredients) => {
+  return ingredients.map((ingredient) => {
+    const listItemEl = document.createElement("li");
+    listItemEl.textContent = ingredient;
+    listItemEl.classList.add("item");
+
+    return listItemEl;
+  });
+};
+
+const items = onListItemElement(ingredients);
+
+ulEl.append(...items);

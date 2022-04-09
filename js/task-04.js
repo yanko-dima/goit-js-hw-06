2a77d8afaa7ecbdb5435d4403c045f13d71b56a8
Yanko Dima <yanko.dmitriy@gmail.com>
2022-04-09 13:23:04 +0300

    Task 4 Do

---
+++ js/task-04.js
@@ -0,0 +1,27 @@
+const counterValue = {
+  value: 0,
+
+  onIncrement() {
+      this.value +=1;
+  },
+
+  onDecrement() {
+      this.value -= 1;
+  },
+};
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const counterEl = document.getElementById("value");
+
+decrementBtn.addEventListener('click', function () {
+    counterValue.onDecrement();
+    counterEl.textContent = counterValue.value;
+
+
+});
+
+incrementBtn.addEventListener('click', function () {
+    counterValue.onIncrement();
+    counterEl.textContent = counterValue.value;
+});

0154109962cb394b5a0dfaefb39a7285e3b81180
Yanko Dima <yanko.dmitriy@gmail.com>
2022-04-09 17:29:19 +0300

    Task 9 Do

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorButton = document.querySelector(".change-color");
+const body = document.querySelector("body");
+
+changeColorButton.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  return (body.style.backgroundColor = getRandomHexColor());
+}

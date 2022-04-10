069f68cbb7b5b93ebc295b34e7b02e005c6332b4
Yanko Dima <yanko.dmitriy@gmail.com>
2022-04-10 21:39:53 +0300

    Task 10 Do

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,43 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const boxes = document.querySelector("#boxes");
+const inputEl = document.querySelector("#controls > input");
+const cteateBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+let amount = 0;
+
+cteateBtn.addEventListener("click", onCreateBtn);
+destroyBtn.addEventListener("click", destroyBoxes);
+inputEl.addEventListener("input", onCurrentAmount);
+
+// Функция трекает значене amount
+function onCurrentAmount(event) {
+  amount = Number(event.currentTarget.value);
+  console.log(amount);
+}
+
+// Обраьатываем клик по кнопке создать
+function onCreateBtn() {
+  createBoxes(amount);
+}
+
+// 2. Функция создает <div> c amount,
+function createBoxes(amount) {
+  const markup = new Array(amount)
+    
+  for (let i = 0; i < markup.length; i++) {
+    markup[i] = `<div style="background-color:
+    ${getRandomHexColor()};
+    width: ${30 + i * 10}px;
+    height: ${30 + i * 10}px;"></div>`;
+    
+  }
+  boxes.innerHTML = markup.join('');
+}
+
+// Фукция очищает содержимое div#boxes
+function destroyBoxes() {
+  boxes.innerHTML = "";
 }
